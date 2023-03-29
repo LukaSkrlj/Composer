7510ca5ab2ec94a5b2e33cb495bc44efef5eb27d
LukaSkrlj <luka.skrlj6@gmail.com>
2023-03-29 08:32:33 +0200

    Added sample dialog

--- app/src/main/java/com/example/composer/Sheet.kt
+++ app/src/main/java/com/example/composer/Sheet.kt
@@ -1,9 +1,14 @@
 package com.example.composer
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,6 +22,18 @@ class Sheet : AppCompatActivity() {
         textView = findViewById<TextView>(R.id.textView)
         gestureDetector = ScaleGestureDetector(this, PinchZoomListener())
 
+        findViewById<ImageView>(R.id.imageView).setOnClickListener {
+            val dialog = layoutInflater.inflate(R.layout.edit_note_dialog, null)
+            val editNoteDialog = Dialog(this)
+            editNoteDialog.setContentView(dialog)
+            editNoteDialog.setCancelable(true)
+            editNoteDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            editNoteDialog.show()
+
+            dialog.findViewById<Button>(R.id.button2).setOnClickListener {
+                editNoteDialog.dismiss()
+            }
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -28,9 +45,9 @@ class Sheet : AppCompatActivity() {
         override fun onScale(detector: ScaleGestureDetector): Boolean {
             var scale = detector.scaleFactor
             if (scale > 1) {
-                textView.text = "Zoom out"
-            } else {
                 textView.text = "Zoom in"
+            } else {
+                textView.text = "Zoom out"
             }
             return true
         }
